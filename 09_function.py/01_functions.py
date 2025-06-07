194891f008986c9dfc27b8dc946e22122712b8e0
sayyam-malvi <sayyammalavi2345@gmail.com>
2025-06-07 11:47:00 +0530

    Just to whether my git is working or not

--- 09_function.py/01_functions.py
+++ 09_function.py/01_functions.py
@@ -1,10 +1,11 @@
 def average(a,b,c):
     s = (a+b+c)/3
-    # return s 
+    return s 
 
 first = average(2,3,4)
 second = average(5,6,7)
 
 print(first)
 print(second)
-print(first + second)
+print(int(first) + int(second))
+print(first)
